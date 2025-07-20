7dbdfb788ef216f58705d593adf6808f3442337b
javaSwing <zxdlovejava@gmail.com>
2025-07-20 11:21:57 +0800

    feat: complete base learn

--- chap2/djangotutorial/mysite/settings.py
+++ chap2/djangotutorial/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
+
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- chap2/djangotutorial/polls/models.py
+++ chap2/djangotutorial/polls/models.py
@@ -1,3 +1,8 @@
+import datetime
+
+from django.contrib import admin
+from django.utils import timezone
+
 from django.db import models
 
 # Create your models here.
@@ -5,6 +10,11 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    @admin.display(boolean=True, ordering="pub_date", description="published recently?")
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
